ef59804cc8d6372cba0c9cf961114ba36cadeec6
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-04-04 18:15:58 +0300

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,19 +3,25 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state={
         count:0,
-        tags:['tag1', 'tag2', 'tag3']
+        tags:[]
     };
     styles={
         fontSize:'24px'
     };
+    renderTags(){
+        if(this.state.tags.length==0) return <p>There are no tags!</p>;
+        return (
+        <ul>
+            {this.state.tags.map(item=><li key={item}>{item}</li>)}
+        </ul>
+        );
+    }
     render() { 
         return (
         <React.Fragment>
             <span style={this.styles}className="badge badge-primary m-2">{this.state.count}</span>
             <button className="btn btn-secondary btn-sm">Button Alperen</button>
-            <ul>
-                {this.state.tags.map(item=><li key={item}>{item}</li>)}
-            </ul>
+            {this.renderTags()}
         </React.Fragment>);
     }
 }
