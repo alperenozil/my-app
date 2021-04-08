812396503f91bb9756893ef96be816314802f775
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-04-08 20:34:52 +0300

    Update counters.jsx

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,16 +4,16 @@ class Counters extends Component {
     state = {  
         counters:[{id:1,value:2},{id:2,value:0},{id:3,value:0},{id:4,value:0}]
     }
-    handleDelete=(  )=>{
-        this.setState({ value:this.state.value+1 })
-        console.log('delete clicked',this.state.value);
+    handleDelete = counterId => {
+        console.log('delete clicked ',counterId);
     }
     render() { 
         return ( 
             <div>
-                {this.state.counters.map(counter=>
-                <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} >
-                </Counter>)}
+                {this.state.counters.map(counter=>(
+                <Counter 
+                    key={counter.id} onDelete={this.handleDelete} value={counter.value} >
+                </Counter>))}
             </div>
          );
     }
