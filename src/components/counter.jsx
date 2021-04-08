edc6b2e64c5cdb11120b1eff6e17eb3374b5c307
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-04-08 20:34:50 +0300

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -33,7 +33,7 @@ class Counter extends Component {
                 className="btn btn-secondary btn-sm">Increment
             </button>
             <button 
-                onClick={this.props.onDelete} 
+                onClick={()=>this.props.onDelete(this.props.id)} 
                 className="btn btn-danger btn-sm m-2">Delete
             </button>
             {this.state.tags.length === 0 && 'Please create a new tag'}
